ac6c9968f40ba621be191749ccf97fb5e9f59947
Sandeep Joshi <joshisandeep943@gmail.com>
2023-06-04 23:32:43 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    <div class="container">
+        This is my First PHP Website.
+        <?php
+        echo "This is printed using php";
+        ?>
+    </div>
+    
+</body>
+</html>
